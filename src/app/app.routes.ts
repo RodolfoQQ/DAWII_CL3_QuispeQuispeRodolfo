bccc0f45807d98608c7714d0a9cbcc03ec41d0bf
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-07-03 12:40:41 -0500

    menu con opciones agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DasboardComponent } from './dasboard/dasboard.component';
+import { Pregunta1Component } from './dasboard/pregunta1/pregunta1.component';
+import { Pregunta2Component } from './dasboard/pregunta2/pregunta2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"dasboard",component:DasboardComponent,
+    children:[
+      {path:"pregunta1",component:Pregunta1Component},
+      {path:"pregunta2",component:Pregunta2Component}
+  ] },
+  {path: "", redirectTo: "dasboard", pathMatch: "full"},
+];
